a1a2d9a2f0e650ec3d6190e111cb80648b936211
samuelsontech@gmail.com <samuelsontech@gmail.com>
2022-09-27 10:23:02 +0100

    jsFunctions

---
+++ functions.js
@@ -0,0 +1,6 @@
+function greet(theName,favC) {
+  return `Hello, my name is ${theName} and my favourite color is ${favC}`
+}
+
+let result = greet('John Grisham', 'Onion Green')
+console.log(result)
