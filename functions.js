e8bae443d90b9b8c195cc987cf90a4b89f2ddbd7
samuelsontech@gmail.com <samuelsontech@gmail.com>
2022-10-01 17:37:13 +0100

    Temperature Conversion from Celsius to Fahrenheit.

--- functions.js
+++ functions.js
@@ -1,6 +1,19 @@
-function greet(theName,favC) {
-  return `Hello, my name is ${theName} and my favourite color is ${favC}`
-}
+// function greet(theName,favC) {
+//   return `Hello, my name is ${theName} and my favourite color is ${favC}`
+// }
 
-let result = greet('John Grisham', 'Onion Green')
-console.log(result)
+// let result = greet('John Grisham', 'Onion Green')
+// console.log(result)
+
+// Current day temperature forecast in kelvin units.
+const kelvin = 293; 
+
+// Celsius variable.
+const Celsius = kelvin - 273;
+
+//Fahnrenheit value
+let Fahrenheit = Celsius * (9/5) + 32;
+
+// Round the result of Celsius to Fahnrenheit conversion.
+Fahrenheit = Math.floor(Fahrenheit)
+console.log(Fahrenheit)
