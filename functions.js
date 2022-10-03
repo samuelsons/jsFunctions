7d266493d04ab4dac12b76753cdc81f5e5f42046
samuelsontech@gmail.com <samuelsontech@gmail.com>
2022-10-03 18:03:15 +0100

    Loops & Conditions

--- functions.js
+++ functions.js
@@ -9,16 +9,38 @@
 /* Temperature Conversion from Celsius to Fahrenheit */
 /*---------------------------------------------------*/
 // Current day temperature forecast in kelvin units.
-const kelvin = 293; 
+// const kelvin = 293; 
 
-// Celsius variable.
-const Celsius = kelvin - 273;
+// // Celsius variable.
+// const Celsius = kelvin - 273;
 
-//Fahnrenheit value
-let Fahrenheit = Celsius * (9/5) + 32;
+// //Fahnrenheit value
+// let Fahrenheit = Celsius * (9/5) + 32;
 
-// Round the result of Celsius to Fahnrenheit conversion.
-Fahrenheit = Math.floor(Fahrenheit)
-console.log(Fahrenheit)
+// // Round the result of Celsius to Fahnrenheit conversion.
+// Fahrenheit = Math.floor(Fahrenheit)
+// console.log(Fahrenheit)
 
 /* ---End of Temperature Program--- */
+
+
+// Human to Dog Age Conversion program.
+// let myAge = 37;
+
+// //Computer the first 2years of a dog and store in variable.
+// let earlyYears  = 2;
+// let dogYears = ((myAge - 2) * 4) + 21;
+// console.log(`My age in dog years is: ${dogYears}years`);
+
+
+//Loops & Conditional 
+/*+++++++++++++++++*/
+
+//For Loop - Log a value in console 5x
+
+//Initialize an index.
+// Condition for iteration.
+// 
+for (let i = 0; i < 6; i++) {
+    console.log('Loop' + i)
+}
